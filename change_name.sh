c0cbedc1c129c4da1597ce754becd95e2620a039
Richard <richard.be.jamin@gmail.com>
2014-03-07 20:45:20 -0600

    fixed font size bug in underscores theme

--- change_name.sh
+++ change_name.sh
@@ -12,3 +12,15 @@ find ./wp -type f -print0 | xargs -0 sed -i "s/underscores/$projectname/g"
 echo - Renaming Underscore theme to $projectname.
 [ ! -d wp/wp-content/themes/$projectname ] && git mv wp/wp-content/themes/underscores wp/wp-content/themes/$projectname
 sed -i "s/Theme Name: underscores/Theme Name: $projectname/" wp/wp-content/themes/$projectname/style.css
+
+
+# Create uploads directory
+
+
+# scp -r /var/www/amazingrace/wp/wp-content/uploads/* richard@gurustudev.com:/var/www/wp-uploads/amazingrace
+# ln -s /var/www/wp-uploads/amazingrace ./uploads
+# cd /var/www/wp-uploads/amazingrace
+# ln -s /var/www/wp-uploads/amazingrace ./uploads
+# chown -R gurustu:gurustu amazingrace
+# sudo chown -R gurustu:gurustu amazingrace
+# sudo chmod -R 777 amazingrace
