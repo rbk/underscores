7d00b66893c434712a3d39f2aa2b63f8e11a8b07
Richard <richard.be.jamin@gmail.com>
2014-03-11 16:56:19 -0500

    added link to sass stylesheet

--- wp/wp-content/themes/underscores/functions.php
+++ wp/wp-content/themes/underscores/functions.php
@@ -86,6 +86,8 @@ add_action( 'widgets_init', 'underscores_widgets_init' );
 function underscores_scripts() {
 	wp_enqueue_style( 'underscores-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'underscores-sass', get_template_directory_uri() . '/css/application.css' );​
+
 	wp_enqueue_script( 'underscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'underscores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
