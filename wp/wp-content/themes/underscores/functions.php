b120aefa4d1c9697f9535dc948250f02e688c0e7
Richard <richard.be.jamin@gmail.com>
2014-03-20 18:16:25 -0500

    added modernizr

--- wp/wp-content/themes/underscores/functions.php
+++ wp/wp-content/themes/underscores/functions.php
@@ -88,6 +88,8 @@ function underscores_scripts() {
 
 	wp_enqueue_style( 'underscores-sass', get_template_directory_uri() . '/css/application.css' );​
 
+	wp_enqueue_script( 'modernizer', get_template_directory_uri() . '/js/modernizr.2.7.1.js', array(), '20120206', true );
+
 	wp_enqueue_script( 'underscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'underscores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
